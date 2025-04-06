36325d6d7a3ad8f8e1d9e572ceb4eb01a4f08bb8
Kim Tran <kimtran@Kims-MacBook-Air.local>
2025-04-06 13:07:47 -0700

    background

--- frontend/src/pages/RevSearch.tsx
+++ frontend/src/pages/RevSearch.tsx
@@ -7,9 +7,15 @@ const RevSearch = () => {
   const [results, setResults] = useState<{ name: string; predicted_price: number }[]>([]);
 
   return (
-    <div className="w-screen h-screen bg-red-500 flex justify-top items-center flex-col">
-        <button className="absolute top-[2vh] left-[2vw] w-[10vw] rounded-2xl border-gray-400 bg-white hover:cursor-pointer" onClick={() => nav('/')}>Back To Home</button>
-        <p className="text-[5vw]">Type a Price!</p>
+    <div className="w-screen h-screen flex justify-top items-center flex-col">
+      {/* Background GIF */}
+      <img
+        src="/pokemon.gif"
+        alt="pokemon gif"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-5"
+      />
+        <button className="absolute top-[2vh] left-[2vw] w-[10vw] rounded-2xl border-white-400 bg-white hover:cursor-pointer" onClick={() => nav('/')}>Back To Home</button>
+        <p className="mt-[5vh] text-[5vw] text-white">Type a Price!</p>
 
         <PriceSearch onResults={setResults} />
 
